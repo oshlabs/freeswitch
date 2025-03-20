2c7b29161128662dd36e75474e7925221a94fd4a
Leon de Rooij <leon@exquisip.nl>
2025-03-20 07:59:24 +0100

    no listen, no accept

--- src/mod/poc/mod_poc_sctp/mod_poc_sctp.c
+++ src/mod/poc/mod_poc_sctp/mod_poc_sctp.c
@@ -38,7 +38,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_poc_sctp_load);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_poc_sctp_shutdown);
 SWITCH_MODULE_DEFINITION(mod_poc_sctp, mod_poc_sctp_load, mod_poc_sctp_shutdown, NULL);
 
-#define SCTP_PORT 5000
+#define SCTP_PORT 5555
 #define MAX_EVENTS 10
 #define MAX_BUFFER 1024
 
@@ -52,21 +52,19 @@ static struct {
 	switch_mutex_t *mutex;
 } globals;
 
+#if 0
 static switch_status_t sctp_send_message(int client_fd, const char *message) {
 	ssize_t sent = sctp_sendmsg(client_fd, message, strlen(message), 
 							   NULL, 0, 0, 0, 0, 0, 0);
 	return (sent > 0) ? SWITCH_STATUS_SUCCESS : SWITCH_STATUS_FALSE;
 }
+#endif
 
 static void *SWITCH_THREAD_FUNC sctp_server_thread(switch_thread_t *thread, void *obj)
 {
 	struct epoll_event events[MAX_EVENTS];
 	char buffer[MAX_BUFFER];
-	int nfds, i, client_fd;
-	struct sockaddr_in client_addr;
-	socklen_t addr_len;
-	int flags;
-	struct epoll_event ev;
+	int nfds, i;
 	ssize_t len;
 
 	while (globals.running) {
@@ -75,35 +73,31 @@ static void *SWITCH_THREAD_FUNC sctp_server_thread(switch_thread_t *thread, void
 
 		for (i = 0; i < nfds; i++) {
 			if (events[i].data.fd == globals.server_fd) {
-				// New connection
-				addr_len = sizeof(client_addr);
-				client_fd = accept(globals.server_fd, (struct sockaddr *)&client_addr, &addr_len);
-				
-				if (client_fd < 0) continue;
+				struct sockaddr_in peer_addr;
+				socklen_t peer_len = sizeof(peer_addr);
+				struct sctp_sndrcvinfo sinfo;
+				int flags;
 
-				// Set non-blocking
-				flags = fcntl(client_fd, F_GETFL, 0);
-				fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
+				while ((len = sctp_recvmsg(globals.server_fd, buffer, MAX_BUFFER-1,
+										   (struct sockaddr*)&peer_addr, &peer_len,
+										   &sinfo, &flags)) > 0) {
+					buffer[len] = '\0';
+					switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO,
+									  "SCTP received: %s\n", buffer);
 
-				ev.events = EPOLLIN | EPOLLET;
-				ev.data.fd = client_fd;
-				epoll_ctl(globals.epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
-			} else {
-				// Data from existing connection
-				client_fd = events[i].data.fd;
-				len = sctp_recvmsg(client_fd, buffer, MAX_BUFFER, NULL, 0, NULL, NULL);
-
-				if (len <= 0) {
-					epoll_ctl(globals.epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
-					close(client_fd);
-					continue;
+					// Respond to exact peer (mandatory!)
+					sctp_sendmsg(globals.server_fd, "ok", 2,
+								 (struct sockaddr*)&peer_addr, peer_len,
+								 sinfo.sinfo_ppid, sinfo.sinfo_flags, sinfo.sinfo_stream, 0, 0);
 				}
 
-				buffer[len] = '\0';
-				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "SCTP received: %s\n", buffer);
-				
-				// Send "ok" response
-				sctp_send_message(client_fd, "ok");
+				if (len < 0 && (errno != EAGAIN && errno != EWOULDBLOCK)) {
+					switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR,
+									  "recvmsg error: %s\n", strerror(errno));
+				}
+
+
+
 			}
 		}
 	}
@@ -134,10 +128,10 @@ static switch_status_t init_sctp_server(void)
 		return SWITCH_STATUS_FALSE;
 	}
 
-	if (listen(globals.server_fd, 5) < 0) {
-		close(globals.server_fd);
-		return SWITCH_STATUS_FALSE;
-	}
+	//if (listen(globals.server_fd, 5) < 0) {
+	//	close(globals.server_fd);
+	//	return SWITCH_STATUS_FALSE;
+	//}
 
 	globals.epoll_fd = epoll_create1(0);
 	if (globals.epoll_fd < 0) {
@@ -145,7 +139,7 @@ static switch_status_t init_sctp_server(void)
 		return SWITCH_STATUS_FALSE;
 	}
 
-	ev.events = EPOLLIN | EPOLLET;
+	ev.events = EPOLLIN; // add later: | EPOLLET;
 	ev.data.fd = globals.server_fd;
 	epoll_ctl(globals.epoll_fd, EPOLL_CTL_ADD, globals.server_fd, &ev);
 
